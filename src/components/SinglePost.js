26e2556e470c09df9942aa541f94bc6103c21dfc
Cassandra <cparisi1290@gmail.com>
2021-07-15 01:45:38 -0500

    request fetch from sanity, set up styling for page

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import sanityClient from '../client';
+import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from '@sanity/block-content-to-react'
 import image from '../pink-flower.jpg';
 
-const SinglePost = () => {
-
-    return (
-        <div>
-            <img src={image} alt='pink flower on branch' className='absolute object-cover w-full h-full'/>
-        </div>
-    )
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
 }
 
-export default SinglePost
+export default function SinglePost() {
+    const [singlePost, setSinglePost] = useState(null);
+    const { slug } = useParams();
+
+    useEffect(() => {
+        sanityClient
+          .fetch(
+            `*[slug.current == '${slug}']{
+                title,
+                _id,
+                slug,
+                mainImage{
+                    asset->{
+                        _id,
+                        url
+                    }
+                },
+                body,
+                'name': author->name,
+                'authorImage': author->image
+        }`)
+        .then((data) => setSinglePost(data[0]))
+        .catch(console.error);
+    }, [slug]);
+
+    if (!singlePost) return <div>Loading...</div>;
+
+    return (
+        <main className='min-h-screen p-12 project-image'>
+            <article className='container shadow-lg mx-auto bg-green-100 rounded-lg'>
+                <header className='relative'>
+                    <div className='absolute h-full w-full flex items-center justify-center p-8'>
+                        <div className='bg-green-800 text-green-100 bg-opacity-75 rounded p-12'>
+                            <h1 className='cursive text-3xl lg:text-6xl mb-4'>
+                                {singlePost.title}
+                            </h1>
+                            <div className='flex justify-center text-gray-800'>
+                                <img src={urlFor(singlePost.authorImage).url()} alt={singlePost.name} className='w-20 h-16 rounded-full'/>
+                                <p className='cursive text-green-100 flex items-center pl-2 text-2xl'> {singlePost.name}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={singlePost.mainImage.asset.url} alt={singlePost.title} className='w-full object-cover rounded-t' style={{ height: '400px' }}/>
+                </header>
+                <div className='px-16 lg:px-48 py-12 lg:py-20 prose lg:prose-xl max-w-full'><BlockContent blocks={singlePost.body} projectId='a6wykfht' dataset='production'/>
+                </div>
+            </article>
+        </main>
+    );
+}
+
