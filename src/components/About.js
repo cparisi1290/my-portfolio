fc2e5db3cd289b1f3c351a9e80737fef590b66cd
Cassandra <cparisi1290@gmail.com>
2021-07-16 14:01:22 -0500

    create fetch request with sanity, set up DOM

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,50 @@
-import React from 'react'
-import image from '../pink-flower.jpg'
+import React, { useEffect, useState } from 'react';
+import sanityClient from '../client';
+import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from '@sanity/block-content-to-react'
+import flower from '../pink-flowerog.jpg';
 
-const About = () => {
-    return (
-        <div>
-            <img src={image} alt='pink flower on branch' className='absolute object-cover w-full h-full'/>
-        </div>
-    )
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
 }
 
-export default About
+export default function About() {
+  const [author, setAuthor] = useState(null);
+
+    useEffect(() => {
+        sanityClient
+          .fetch(
+            `*[_type == "author"]{
+                name,
+                bio,
+                "authorImage": image.asset->url
+          }`
+          )
+          .then((data) => setAuthor(data[0]))
+          .catch(console.error);
+      }, []);
+
+      if (!author) return <div>Loading...</div>;
+
+    return (
+        <main className='relative'>
+            <img src={flower} alt='pink flower on branch' className='absolute object-cover w-cover h-cover'/>
+            <div className='p-10 lg:pt-32 container mx-auto relative'>
+                <section className='bg-muave rounded-lg lg:flex p-16' >
+                    <img src={urlFor(author.authorImage).url()} className='rounded w-auto h-32 lg:w-auto lg:h-64 mr-8' alt={author.name}/>
+                    <div className='text-lg flex flex-col justify-center'>
+                        <h1 className='cursive text-6xl featured-posts'>Hi there! I'm {author.name}.
+                        </h1>
+                        <div className='prose lg:prose-xl featured-posts'>
+                            <BlockContent 
+                                blocks={author.bio}
+                                projectId="a6wykfht"
+                                dataset="production"/>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )
+}
